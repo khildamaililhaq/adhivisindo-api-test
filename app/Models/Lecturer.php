f0cb1c612b7be4b2f331b9af9e2460545822294e
Khilda maililhaq <khildamaililhaq@pop-os.localdomain>
2025-09-24 21:09:08 +0700

    fix file upload

--- app/Models/Lecturer.php
+++ app/Models/Lecturer.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Support\Facades\Storage;
 
 class Lecturer extends Model
 {
@@ -19,5 +20,10 @@ class Lecturer extends Model
     public function learningModules(): BelongsToMany
     {
         return $this->belongsToMany(LearningModule::class, 'lecturer_learning_module');
+    }
+
+    public function getPhotoAttribute($value)
+    {
+        return $value ? Storage::url($value) : null;
     }
 }
